19d64397beae06d8b713d67bf913169bf3a5a11f
hassan0599 <hassan0599@gmail.com>
2023-07-31 04:12:55 -0400

    Designed and developed a learn together android app

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnTogetherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticle() {
+    Article(
+        painter = painterResource(R.drawable.bg_compose_background),
+        title = stringResource(R.string.title),
+        shortDescription = stringResource(R.string.short_description),
+        longDescription = stringResource(R.string.long_description)
+    )
+}
+
+@Composable
+fun Article(
+    painter: Painter,
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    modifier: Modifier = Modifier
+    ) {
+    Column(modifier = modifier) {
+        Image(
+            painter = painter,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = shortDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp
+                )
+        )
+        Text(
+            text = longDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LearnTogetherTheme {
+        ComposeArticle()
+    }
+}
